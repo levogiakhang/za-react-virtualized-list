8fb1bf6dc1bc1eacd945c1335f2fcffdabf50c50
Gia Khang <levogiakhang@gmail.com>
2019-06-26 15:59:45 +0700

    Update data

--- src/utils/FakeData.js
+++ src/utils/FakeData.js
@@ -1,11 +1,7 @@
 export const fakeData = {
-  "itemId": 'giakhang',
-  "registered":
-    {
-      "date": "2009-01-25T09:51:56Z",
-    },
-  "picture":
-    {
-      "thumbnail": "https://randomuser.me/api/portraits/thumb/men/74.jpg"
-    },
+  itemId: 'id_',
+  userName: 'user_',
+  msgContent: "",
+  avatar: "https://randomuser.me/api/portraits/thumb/men/",
+  timestamp: "2009-01-25T09:51:56Z",
 };
