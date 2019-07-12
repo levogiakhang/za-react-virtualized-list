4b64d14448bb05e2b437f88bdaa68548d4f19ae8
Gia Khang <levogiakhang@gmail.com>
2019-07-12 17:00:28 +0700

    :sparkles: Add data view model

---
+++ src/ViewModel/DataViewModel.js
@@ -0,0 +1,125 @@
+// @flow
+
+import { NOT_FOUND } from "../utils/value";
+
+class DataViewModel {
+  constructor(data) {
+    this.data = data;
+
+    // for purpose quick look-up
+    this.dataMap = new Map();
+    this.init();
+  }
+
+  init() {
+    if (Array.isArray(this.data)) {
+      data.forEach((item) => {
+        this.dataMap.set(item.itemId, item);
+      })
+    } else {
+      console.error('The initialized data is NOT an array');
+    }
+  }
+
+  clearData() {
+    this.data = [];
+    this.dataMap.clear();
+  }
+
+  updateNewData(data) {
+    this.setData(data);
+    this.init();
+  }
+
+  insertItem(index: number, item: Object) {
+    if (
+      Array.isArray(this.data) &&
+      this._isValidIndex(index) &&
+      !this._isIdAlready(item.itemId)
+    ) {
+      this.data.splice(index, 0, item);
+      this.dataMap.set(item.itemId, item);
+    }
+  }
+
+  insertItems(index: number, arrItem: Array) {
+    if (
+      Array.isArray(this.data) &&
+      Array.isArray(arrItem) &&
+      this._isValidIndex(index)
+    ) {
+      for (let i = 0; i <= arrItem.length - 1; i++) {
+        if (!this._isIdAlready(arrItem[i].itemId)) {
+          this.data.splice(index, 0, arrItem[i]);
+          this.dataMap.set(arrItem[i].itemId, arrItem[i]);
+        }
+      }
+    }
+  }
+
+  deleleItem(index: number) {
+    if (
+      Array.isArray(this.data) &&
+      this._isValidIndex(index)
+    ) {
+      this.data.splice(index, 1);
+      this.dataMap.delete(this.data[index].itemId);
+    }
+  }
+
+  addItemTop(item: Object) {
+    this.insertItem(0, item);
+  }
+
+  appendTop(arrItem: Array) {
+    if (
+      Array.isArray(this.data) &&
+      Array.isArray(arrItem)
+    ) {
+      for (let i = arrItem - 1; i >= 0; i--) {
+        if (!this._isIdAlready(arrItem[i].itemId)) {
+          this.data.unshift(arrItem[i]);
+          this.dataMap.set(arrItem[i].itemId, arrItem[i]);
+        }
+      }
+    }
+  }
+
+  addItemBottom(item: Object) {
+    this.data.length === 0 ?
+      this.insertItem(0, item) :
+      this.insertItem(this.data.length - 1, item);
+  }
+
+  appendBottom(arrItem: Array) {
+    this.data.length === 0 ?
+      this.insertItems(0, arrItem) :
+      this.insertItems(this.data.length - 1, arrItem);
+  }
+
+  getItem(itemId: string): Object {
+    return this.dataMap.has(itemId) ? this.dataMap.get(itemId) : NOT_FOUND;
+  }
+
+  _isValidIndex(index: number): boolean {
+    return (
+      typeof index === 'number' &&
+      index < this.data.length &&
+      index >= 0
+    );
+  }
+
+  _isIdAlready(id: string): boolean {
+    return this.dataMap.has(id);
+  };
+
+  getData() {
+    return this.data;
+  }
+
+  setData(newData: Array) {
+    this.data = newData;
+  }
+}
+
+export default DataViewModel;
